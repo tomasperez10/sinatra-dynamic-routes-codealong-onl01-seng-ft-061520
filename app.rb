f3fa5fc5e57cfe755cd36b6b63e62d525f46ed39
Tomas Perez <tomasitopg6@gmail.com>
2020-08-07 04:31:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,8 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
 
   get "/multiply/:num1/:num2" do
-    @num1 = params[:num1]
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
 
 
 end
